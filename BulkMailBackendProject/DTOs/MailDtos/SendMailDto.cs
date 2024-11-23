c6a9a39b3a53bb1454d4a2de288468348cf1a828
elvisaghaulor <aghaulorelvis@gmail.com>
2024-11-24 00:55:08 +0100

    send mail updates

--- BulkMailBackendProject/DTOs/MailDtos/SendMailDto.cs
+++ BulkMailBackendProject/DTOs/MailDtos/SendMailDto.cs
@@ -7,10 +7,10 @@ public class SendMailDto
     public string mailBody { get; set; }
     public string mailSubject { get; set; }
     public List<CustomeMailAddress> receiversList { get; set; }
-    public string copiedList { get; set; }
+    public string? copiedList { get; set; }
 }
 
 public class CustomeMailAddress{
     public string emailAddress { get; set; }
-    public string emailName { get; set; }
+    public string? emailName { get; set; }
 }
